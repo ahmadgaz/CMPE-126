0abab72c37f89ead7327a9dcc639a642dc1c5a4f
Ahmad Gazali <105146133+ahmadgaz@users.noreply.github.com>
2023-03-07 13:47:51 -0800

    Create SavingsAccount.h

---
+++ Lab/Lab3_1/SavingsAccount.h
@@ -0,0 +1,13 @@
+#ifndef SAVINGSACCOUNT_H
+#define SAVINGSACCOUNT_H
+#include "Account.h"
+
+class SavingsAccount: public Account {
+public:
+	SavingsAccount(double, double);
+	double calculateInterest() const;
+private:
+	double interestRate;
+};
+
+#endif
